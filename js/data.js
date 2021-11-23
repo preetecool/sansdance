45015d66d64e82a27ba0db49216d1aa7f4313fc1
Preet <preet@creal.ca>
2021-11-23 12:06:14 -0500

    deploy changes

--- js/data.js
+++ js/data.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // In this file we have some data that the other source files will use.
 // Most of this data is stored in constants.
 // Constants are just variables that never change. By convention,
@@ -18,24 +17,3 @@ const MAX_ENEMIES = 3;
 // These constants represent the player width and height.
 const PLAYER_WIDTH = 75;
 const PLAYER_HEIGHT = 54;
-=======
-// In this file we have some data that the other source files will use.
-// Most of this data is stored in constants.
-// Constants are just variables that never change. By convention,
-// We write constants with upper case letters.
-
-// The GAME_WIDTH and GAME_HEIGHT constants denote the size
-// of the game area in pixels and is used in engine-utilities.js.
-const GAME_WIDTH = 500;
-const GAME_HEIGHT = 700;
-
-// These constants represent the width and height of an enemy in pixels
-// as well as the maximum number of enemies on screen at any given time.
-const ENEMY_WIDTH = 75;
-const ENEMY_HEIGHT = 60;
-const MAX_ENEMIES = 3;
-
-// These constants represent the player width and height.
-const PLAYER_WIDTH = 75;
-const PLAYER_HEIGHT = 54;
->>>>>>> e01101aaac4567c14497d1c3a84df167bd5b0cf2
